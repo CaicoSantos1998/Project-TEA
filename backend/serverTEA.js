751f2596fa38ed14d2c29ffc6e49f972087d40ac
CS <cs_1998@hotmail.com>
2025-08-07 15:34:13 -0300

    Update in file serverTEA.js, adding (public/html

--- backend/serverTEA.js
+++ backend/serverTEA.js
@@ -11,7 +11,7 @@ const db = mysql.createPool({
 	uri: 'mysql://root:BiSViYxCHnnPUidqCYDyvXpCzMaMjuff@shuttle.proxy.rlwy.net:35093/railway'
 });
 
-st.use(express.static(path.join(__dirname, 'public')));
+st.use(express.static(path.join(__dirname, 'public/html')));
 
 async function testConnection() {
 	try {
