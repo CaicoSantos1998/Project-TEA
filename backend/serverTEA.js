8fce0bc4bb8de3c8f4c8f81116878ffa9ced8227
CS <cs_1998@hotmail.com>
2025-08-07 16:47:09 -0300

    Correcting path of index.html in server

--- backend/serverTEA.js
+++ backend/serverTEA.js
@@ -27,7 +27,7 @@ async function testConnection() {
 testConnection();
 
 st.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public/html/index.html'));
+    res.sendFile(path.join(__dirname, 'public/html', 'index.html'));
 });
 
 st.post('/form', async (req, res) => {
