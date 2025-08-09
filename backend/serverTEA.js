dddf5c43e23beef58c5c75041577e078e309f16f
CS <cs_1998@hotmail.com>
2025-08-09 15:26:45 -0300

    Update to check login and file for download

--- backend/serverTEA.js
+++ backend/serverTEA.js
@@ -5,7 +5,6 @@ const path = require('path');
 const session = require('express-session');
 const PDFDocument = require('pdfkit');
 const { error } = require('console');
-import PDFDocument from 'pdfkit';
 
 const st = express();
 
@@ -303,13 +302,12 @@ st.get('/document.pdf', async (req, res) => {
         doc.moveDown();
 
         if (dados.length > 0) {
-            // Cabeçalho
             Object.keys(dados[0]).forEach(key => {
                 doc.fontSize(12).text(key, { continued: true }).text(' | ', { continued: true });
             });
+
             doc.moveDown();
 
-            // Linhas
             dados.forEach(item => {
                 Object.values(item).forEach(value => {
                     doc.fontSize(10).text(String(value), { continued: true }).text(' | ', { continued: true });
